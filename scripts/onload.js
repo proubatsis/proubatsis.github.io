a443cf5316939f088b88446c110926d9da8cb1a1
Panagiotis Roubatsis <proubatsis@gmail.com>
2016-05-07 10:52:48 -0400

    Put scripts and stylsheets into their own files.

---
+++ scripts/onload.js
@@ -0,0 +1,27 @@
+window.onload = function()
+{
+   // Create and draw the programming language proficiency bar graph
+   var canvas = document.getElementById("programmingChartCanvas");
+   var chart = new Chart(canvas, {
+    type: "bar",
+    data: {
+     labels: ["C++", "Java", "Python", "C#", "JavaScript", "HTML/CSS", "F#", "Assembly"],
+     datasets: [{
+      label: "Proficiency %",
+      backgroundColor: "rgba(39, 174, 96, 0.8)",
+      data: [90, 85, 82, 75, 65, 60, 50, 25]
+     }]
+    },
+    options: {
+     scales: {
+      yAxes: [{
+       ticks: {
+        beginAtZero: true,
+        max: 100
+        }
+      }]
+     }
+    }
+   });
+}
+
